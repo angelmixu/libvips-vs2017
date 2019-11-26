81bfb26a1737e83c2ecc55d2d5d9cb5e99752831
Angel Sánchez <angelsanchez@inedit.com>
2019-11-26 13:23:24 +0100

    add macOS build script

---
+++ BUILD_macos.sh
@@ -0,0 +1,91 @@
+#!/bin/bash
+set -e
+
+libPath=lib
+configuration=Release
+
+shopt -s nocasematch
+while test $# -gt 0
+do
+    case "$1" in
+        forceclean|clean) forceclean=1 #actionList=(clean build)
+            ;;
+        debug) libPath=debug/lib
+				configuration=Debug
+            ;;
+        release) libPath=lib
+				configuration=Release
+            ;;
+        *) echo "wrong argument: $1"
+			exit 1
+            ;;
+    esac
+    shift
+done
+
+
+# set deployment target to macOS 10.9:
+export MACOSX_DEPLOYMENT_TARGET=10.9
+
+## $ brew install autoconf automake gtk-doc gobject-introspection
+
+
+# Alternatively, you may set the environment variables REQUIRED_CFLAGS
+# and REQUIRED_LIBS to avoid the need to call pkg-config.
+
+export OSS_BINARIES_PATH=~/code/sampi/ossBinaries/macOS
+export REQUIRED_CFLAGS="-I$OSS_BINARIES_PATH/include"
+export REQUIRED_LIBS="-framework CoreServices -framework CoreFoundation -framework CoreGraphics -framework CoreText -liconv -L$OSS_BINARIES_PATH/$libPath -lintl -lffi -lglib-2.0 -lgobject-2.0 -lgio-2.0 -lgmodule-2.0 -lgthread-2.0 -lpango-1 -lpangoft2-1 -lfontconfig -llcms -lpng -lturbojpeg -llibexif -ltiff -lz -lpcre -llzma -lharfbuzz -lfreetype -lbz2"
+export GTHREAD_CFLAGS="-I$OSS_BINARIES_PATH/include"
+export GTHREAD_LIBS="-L$OSS_BINARIES_PATH/$libPath"
+
+if [ "$configuration" == "Debug" ]; then
+	export REQUIRED_LIBS="-framework CoreServices -framework CoreFoundation -framework CoreGraphics -framework CoreText -liconv -L$OSS_BINARIES_PATH/$libPath -lintl -lffi -lglib-2.0 -lgobject-2.0 -lgio-2.0 -lgmodule-2.0 -lgthread-2.0 -lpango-1 -lpangoft2-1 -lfontconfig -llcmsd -lpng -lturbojpeg -llibexif -ltiff -lz -lpcre -llzma -lharfbuzz -lfreetyped -lbz2d"
+fi
+
+# also there's a script from glib needed for building vips, we should add the
+# last path to the environment variable in order for vips to build correctly:
+
+export PATH=$OSS_BINARIES_PATH:$PATH
+
+export JPEG_CFLAGS="-I$OSS_BINARIES_PATH/include"
+export JPEG_LIBS="-L$OSS_BINARIES_PATH/$libPath"
+export EXIF_CFLAGS="-I$OSS_BINARIES_PATH/include"
+export EXIF_LIBS="-L$OSS_BINARIES_PATH/$libPath"
+export PNG_CFLAGS="-I$OSS_BINARIES_PATH/include"
+export PNG_LIBS="-L$OSS_BINARIES_PATH/$libPath"
+export TIFF_CFLAGS="-I$OSS_BINARIES_PATH/include"
+export TIFF_LIBS="-L$OSS_BINARIES_PATH/$libPath"
+export PANGOFT2_CFLAGS="-I$OSS_BINARIES_PATH/include"
+export PANGOFT2_LIBS="-L$OSS_BINARIES_PATH/$libPath"
+export ZLIB_CFLAGS="-I$OSS_BINARIES_PATH/include"
+export ZLIB_LIBS="-L$OSS_BINARIES_PATH/$libPath"
+export LCMS_CFLAGS="-I$OSS_BINARIES_PATH/include"
+export LCMS_LIBS="-L$OSS_BINARIES_PATH/$libPath"
+
+if [ "$configuration" == "Debug" ]; then
+	CFLAGS="$CFLAGS -g -O0" 
+	CPPFLAGS="$CPPFLAGS -g -O0" 
+	LDFLAGS="$LDFLAGS -g"
+fi
+
+export DESTINATION_PATH=~/code/sampi/ossBinaries/custom/macOS
+
+pushd srclibvips
+
+# Autogen calls ./configure
+
+sh autogen.sh --enable-static=no --enable-shared=yes --enable-debug=no --without-magick --without-OpenEXR --without-libwebp --without-heif
+
+make -j 4
+
+cp -Ra libvips/.libs/libvips.*dylib cplusplus/.libs/libvips-cpp.*dylib "$DESTINATION_PATH/$configuration"
+mkdir -p $DESTINATION_PATH/include/vips
+cp -Ra libvips/include/vips/*.h $DESTINATION_PATH/include/vips
+cp -Ra cplusplus/include/vips/*.h cplusplus/include/vips/vips8 $DESTINATION_PATH/include/vips/
+
+popd
+
+g++ -dynamiclib -o $DESTINATION_PATH/$configuration/libvipsCC.dylib -install_name @rpath/libvipsCC.dylib libvipsCC/VImage.cc libvipsCC/VError.cc libvipsCC/VDisplay.cc libvipsCC/VMask.cc -Isrclibvips/libvips/include/ -I$OSS_BINARIES_PATH/include -IlibvipsCC/include -lvips -lgobject-2 -L$OSS_BINARIES_PATH/$libPath -Lsrclibvips/libvips/.libs
+cp -Ra libvipsCC/include/vips/*.h libvipsCC/include/vips/vips $DESTINATION_PATH/include/vips/
+
